1f003e8aeabcef5c11dcd47f3b837b3c6537363b
PavleMatijasevic <matijasevic.pavle99@gmail.com>
2021-01-26 23:06:48 +0100

    Izmenio automate

--- automati/automat2.py
+++ automati/automat2.py
@@ -6,13 +6,12 @@ stanje = 'ADE'
 zavrsno = 'C'
 prelaz = {('ADE','a'):'B', ('ADE', 'b'):'C', ('ADE', 'c'):'ADE', ('B', 'b'):'ADE'}
 
-while True:
+rec = input("Unesite celu rec: ")
+
+for c in rec:
     try:
-        c = input("Unesite a ili b ili c: ")
         if(c!='a' and c!='b' and c!='c'):
             raise ValueError("Pogresan karakter!")
-    except EOFError:
-        break;
     except ValueError as e:
         sys.exit(e)
     if prelaz.get((stanje, c)) is None:
@@ -22,3 +21,15 @@ if stanje == zavrsno:
     print("Rec se nalazi u jeziku!")
 else:
     print("Rec se ne nalazi u jeziku!")
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
